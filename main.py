e7e1f37fb0848a22ab93c1e90706b0cd912048af
Andy Woods <andytwoods@users.noreply.github.com>
2022-11-11 17:18:20 +0000

    requirements.txt

--- main.py
+++ main.py
@@ -18,43 +18,33 @@ inflector = inflect.engine()
 html = (open("aliceInWonderland/4560944924381563385_11-h-2.htm.xhtml", "r", encoding='utf-8')).read()
 html2 = '''
 <p>
-“I won’t indeed!” said Alice, in a great hurry to change the
-subject of conversation. “Are you—are you fond—of—of
-dogs?” The Mouse did not answer, so Alice went on eagerly: “There
-is such a nice little dog near our house I should like to show you! A little
-bright-eyed terrier, you know, with oh, such long curly brown hair! And
-it’ll fetch things when you throw them, and it’ll sit up and beg
-for its dinner, and all sorts of things—I can’t remember half of
-them—and it belongs to a farmer, you know, and he says it’s so
-useful, it’s worth a hundred pounds! He says it kills all the rats
-and—oh dear!” cried Alice in a sorrowful tone, “I’m
-afraid I’ve offended it again!” For the Mouse was swimming away
-from her as hard as it could go, and making quite a commotion in the pool as it
-went.
+Just then her head struck against the roof of the hall: in fact she was now
+more than nine feet high, and she at once took up the little golden key and
+hurried off to the garden door.
 </p>
 <p>
-So she called softly after it, “Mouse dear! Do come back again, and we
-won’t talk about cats or dogs either, if you don’t like
-them!” When the Mouse heard this, it turned round and swam slowly back to
-her: its face was quite pale (with passion, Alice thought), and it said in a
-low trembling voice, “Let us get to the shore, and then I’ll tell
-you my history, and you’ll understand why it is I hate cats and
-dogs.”
+Poor Alice! It was as much as she could do, lying down on one side, to look
+through into the garden with one eye; but to get through was more hopeless than
+ever: she sat down and began to cry again.
 </p>
 <p>
-It was high time to go, for the pool was getting quite crowded with the birds
-and animals that had fallen into it: there were a Duck and a Dodo, a Lory and
-an Eaglet, and several other curious creatures. Alice led the way, and the
-whole party swam to the shore.
+“You ought to be ashamed of yourself,” said Alice, “a great
+girl like you,” (she might well say this), “to go on crying in this
+way! Stop this moment, I tell you!” But she went on all the same,
+shedding gallons of tears, until there was a large pool all round her, about
+four inches deep and reaching half down the hall.
 </p>
-<h2><a id="chap01"/>CHAPTER I.<br/>
-Down the Rabbit-Hole</h2>
 <p>
-Alice was beginning to get very tired of sitting by her sister on the bank, and
-of having nothing to do: once or twice she had peeped into the book her sister
-was reading, but it had no pictures or conversations in it, “and what is
-the use of a book,” thought Alice “without pictures or
-conversations?”
+After a time she heard a little pattering of feet in the distance, and she
+hastily dried her eyes to see what was coming. It was the White Rabbit
+returning, splendidly dressed, with a pair of white kid gloves in one hand and
+a large fan in the other: he came trotting along in a great hurry, muttering to
+himself as he came, “Oh! the Duchess, the Duchess! Oh! won’t she be
+savage if I’ve kept her waiting!” Alice felt so desperate that she
+was ready to ask help of any one; so, when the Rabbit came near her, she began,
+in a low, timid voice, “If you please, sir—” The Rabbit
+started violently, dropped the white kid gloves and the fan, and skurried away
+into the darkness as hard as he could go.
 </p>
 '''
 
@@ -68,30 +58,7 @@ doc = nlp(text)
 # for token in doc:
 #     print(token.i, token.text)
 
-coref_chains = doc._.coref_chains
-# coref_chains.print()
-
 p = inflect.engine()
-
-# token.idx
-# len(token.text)
-
-character = 'Alice'
-found_character_tokens = []
-for chain in coref_chains:
-    found_character = False
-    for token_info in chain:
-        token = doc[token_info.root_index]
-        if token.text == character:
-            found_character = True
-            break
-    if found_character:
-        for token_info in chain:
-            token = doc[token_info.root_index]
-            found_character_tokens.append(token)
-
-# last elements dealt with first
-found_character_tokens.sort(key=lambda el: el.idx, reverse=True)
 
 CorrectOrderTuple = namedtuple('CorrectOrderTuple', ['i', 'old', 'new'])
 
@@ -101,8 +68,7 @@ class WordUpdater:
 
     def add(self, token, to):
         info = CorrectOrderTuple(token.idx, token.text, to)
-        self._do_update(info)
-        #self.updates.append(info)
+        self.updates.append(info)
 
     def _do_update(self, info: CorrectOrderTuple):
         global html
@@ -117,7 +83,8 @@ class WordUpdater:
     def update(self):
         self.updates.sort(key=lambda x: x.i)
         while self.updates:
-            self._do_update(self.updates.pop())
+            el = self.updates.pop()
+            self._do_update(el)
 
 
 word_updater = WordUpdater()
@@ -125,27 +92,43 @@ word_updater = WordUpdater()
 
 def update_token(token):
     prev_token = doc[token.i - 1]
-    following_token = doc[token.i + 1]
+    try:
+        following_token = doc[token.i + 1]
+    except IndexError:
+        return
     # print(list(token.children),3333)
     # print(prev_token, token, following_token, 222, following_token.lemma_, following_token.tag_)
+
     to_change = ' '.join([prev_token.text, token.text, following_token.text])
 
-    updates = []
 
-    # MUST be before other modders
+
+    if token.text.lower() in ['i', 'it', 'yourself', 'myself', 'we', 'they']:
+        return
+
     if following_token.tag_[0:2] == 'VB' and following_token.lemma_ in ['has', 'be']:
+
         current_form = following_token.text
         plural_form = inflector.plural(current_form)
+
+        if plural_form == 'bes':
+            plural_form = 'be'  # ffs
+        elif plural_form == 'beens':
+            plural_form = 'been'
+
         if plural_form != current_form:
             word_updater.add(following_token, plural_form)
 
-    if token.tag_ == 'PRP':  # personal pronoun
-        suggested_updated = inflector.plural(token.text)
-        if str(suggested_updated).lower() == 'their':
-            suggested_updated = 'them'
-        word_updater.add(token, suggested_updated)
-    elif token.tag_ == 'PRP$':  # possessive pronoun
-        word_updater.add(token, inflector.plural(token.text))
+    token_tag = token.tag_
+
+    if token_tag[0:3] == 'PRP':  # personal pronoun
+        if token.tag_ == 'PRP':  # personal pronoun
+            suggested_updated = inflector.plural(token.text)
+            if str(suggested_updated).lower() == 'their':
+                suggested_updated = 'them'
+            word_updater.add(token, suggested_updated)
+        elif token.tag_ == 'PRP$':  # possessive pronoun
+            word_updater.add(token, inflector.plural(token.text))
 
     # print('is vb', following_token.tag_, a, following_token.text, '---', to_change, '-----', inflector.plural(following_token.text))
 
@@ -155,11 +138,52 @@ def update_token(token):
     # tag = Penn Treebank tag,
 
 
-for token in found_character_tokens:
-    if token.text == character:
-        continue
-    update_token(token)
+def do_coref():
+    coref_chains = doc._.coref_chains
+    # coref_chains.print()
 
-word_updater.update()
+    character = 'Alice'
+    found_character_tokens = []
+    for chain in coref_chains:
+        found_character = False
+        for token_info in chain:
+            token = doc[token_info.root_index]
+            if token.text == character:
+                found_character = True
+                break
+        if found_character:
+            for token_info in chain:
+                token = doc[token_info.root_index]
+                found_character_tokens.append(token)
+        else:
+            for token_info in chain:
+                token = doc[token_info.root_index]
+
+    for token in found_character_tokens:
+        if token.text == character:
+            continue
+        update_token(token)
+
+    word_updater.update()
+
+do_coref()
+
+
+def do_all():
+    coref_chains = doc._.coref_chains
+    # coref_chains.print()
+
+    found_character_tokens = []
+
+    for token in doc:
+        outcome = update_token(token)
+
+    for token in found_character_tokens:
+        update_token(token)
+
+    word_updater.update()
+
+
+# do_all()
 
 print(html)
