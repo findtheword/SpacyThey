93c2f02189bcc81bb24aaceed44f98311ff093d6
Andy Woods <andytwoods@users.noreply.github.com>
2022-11-28 10:30:08 +0000

    andy working though

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import re
+from collections import namedtuple
 
 html_tags_regexp = re.compile('<.*?>')
 
@@ -12,3 +13,38 @@ def html_replaced_with_spaces(html):
     new_lines_replaces = ' '.join(html_replaced.splitlines())
 
     return new_lines_replaces
+
+
+CorrectOrderTuple = namedtuple('CorrectOrderTuple', ['i', 'old', 'new'])
+
+class WordUpdater:
+    updates = []
+    update_params = {}
+
+    def __init__(self, update_params) -> None:
+        self.update_params = update_params
+        super().__init__()
+
+    def add(self, token, to):
+        info = CorrectOrderTuple(token.idx, token.text, to)
+        self.updates.append(info)
+
+    def do_update(self, info: CorrectOrderTuple, html: str):
+
+        to = info.new
+        old = info.old
+
+        if info.old[0].isupper():
+            to = to[0].upper() + to[1:]
+
+        if self.update_params.get('span', False):
+            to = f"<span title='original: { old }' style='color: purple;'>{ to }</span>"
+
+        return html[0: info.i] + to + html[info.i + len(old):]
+
+    def update(self, html: str):
+        self.updates.sort(key=lambda x: x.i)
+        while self.updates:
+            el = self.updates.pop()
+            html = self.do_update(el, html)
+        return html
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import re
+import shutil
 from collections import namedtuple
 
 import coreferee, spacy  # don't remove coreferee
@@ -6,128 +8,74 @@ import inflect
 import lemminflect
 from spacy import Vocab
 
-from helpers import html_replaced_with_spaces
+from tqdm import tqdm
+from helpers import html_replaced_with_spaces, WordUpdater
 
 nlp = spacy.load('en_core_web_trf')
 nlp.add_pipe('coreferee')
 
 inflector = inflect.engine()
 
+
 # https://github.com/msg-systems/coreferee/issues/19#issuecomment-911522340
 
-html = (open("aliceInWonderland/4560944924381563385_11-h-2.htm.xhtml", "r", encoding='utf-8')).read()
-html = '''<pre>
-     <i>Alice’s Right Foot, Esq.,
-       Hearthrug,
-         near the Fender,</i>
-           (<i>with Alice’s love</i>).
-</pre>
-<p class="noindent">
-Oh dear, what nonsense I’m talking!”
-</p>
-<p>
-Just then her head struck against the roof of the hall: in fact she was now
-more than nine feet high, and she at once took up the little golden key and
-hurried off to the garden door.
-</p>
-<p>
-Poor Alice! It was as much as she could do, lying down on one side, to look
-through into the garden with one eye; but to get through was more hopeless than
-ever: she sat down and began to cry again.
-</p>'''
+def translate(original_html, update_params):
+    text = html_replaced_with_spaces(original_html)
 
-text = html_replaced_with_spaces(html)
+    doc = nlp(text)
+    p = inflect.engine()
 
-# t = [f"{i} {txt}" for i, txt in enumerate(text.split(' '))]
-# print(' '.join(t))
+    word_updater = WordUpdater(update_params)
 
-doc = nlp(text)
+    def update_name(character_name):
+        if not character_name:
+            return
+        from_name = character_name['from']
+        to_name = character_name['to']
 
-# for token in doc:
-#     print(token.i, token.text)
+        TokenMinimal = namedtuple('TokenMinimal', ['idx', 'text'])
 
-p = inflect.engine()
+        for found in re.finditer(from_name, original_html):
+            found: TokenMinimal = TokenMinimal(found.start(), from_name)
+            word_updater.add(found, to_name)
 
-CorrectOrderTuple = namedtuple('CorrectOrderTuple', ['i', 'old', 'new'])
+    update_name(update_params.get('character', None))
 
+    def update_token(token):
 
-class WordUpdater:
-    updates = []
+        try:
+            following_token = doc[token.i + 1]
+        except IndexError:
+            return
 
-    def add(self, token, to):
-        info = CorrectOrderTuple(token.idx, token.text, to)
-        self.updates.append(info)
+        if token.text.lower() in ['i', 'it', 'yourself', 'myself', 'we', 'they']:
+            return
 
+        if following_token.tag_[0:2] == 'VB' and following_token.lemma_ in ['has', 'be']:
 
-    @staticmethod
-    def do_update(info: CorrectOrderTuple):
-        global html
+            current_form = following_token.text
+            plural_form = inflector.plural(current_form, 4)
 
-        to = info.new
+            if plural_form == 'bes':
+                plural_form = 'be'  # ffs
+            elif plural_form == 'beens':
+                plural_form = 'been'
 
-        if info.old[0].isupper():
-            to = to[0].upper() + to[1:]
+            if plural_form != current_form:
+                word_updater.add(following_token, plural_form)
 
-        html = html[0: info.i] + to + html[info.i + len(info.old):]
+        token_tag = token.tag_
 
-    def update(self):
-        self.updates.sort(key=lambda x: x.i)
-        while self.updates:
-            el = self.updates.pop()
-            self.do_update(el)
+        if token_tag[0:3] == 'PRP':  # personal pronoun
+            if token.tag_ == 'PRP':  # personal pronoun
+                suggested_updated = inflector.plural(token.text)
+                if str(suggested_updated).lower() == 'their':
+                    suggested_updated = 'them'
+                word_updater.add(token, suggested_updated)
+            elif token.tag_ == 'PRP$':  # possessive pronoun
+                word_updater.add(token, inflector.plural(token.text))
 
 
-word_updater = WordUpdater()
-
-
-def update_token(token):
-    prev_token = doc[token.i - 1]
-    try:
-        following_token = doc[token.i + 1]
-    except IndexError:
-        return
-    # print(list(token.children),3333)
-    print(prev_token, token, following_token, 222, following_token.lemma_, following_token.tag_)
-
-    to_change = ' '.join([prev_token.text, token.text, following_token.text])
-
-    if token.text.lower() in ['i', 'it', 'yourself', 'myself', 'we', 'they']:
-        return
-
-    if following_token.tag_[0:2] == 'VB' and following_token.lemma_ in ['has', 'be']:
-
-        current_form = following_token.text
-        plural_form = inflector.plural(current_form, 4)
-
-        if plural_form == 'bes':
-            plural_form = 'be'  # ffs
-        elif plural_form == 'beens':
-            plural_form = 'been'
-
-        if plural_form != current_form:
-            word_updater.add(following_token, plural_form)
-
-
-    token_tag = token.tag_
-
-    if token_tag[0:3] == 'PRP':  # personal pronoun
-        if token.tag_ == 'PRP':  # personal pronoun
-            suggested_updated = inflector.plural(token.text)
-            if str(suggested_updated).lower() == 'their':
-                suggested_updated = 'them'
-            word_updater.add(token, suggested_updated)
-        elif token.tag_ == 'PRP$':  # possessive pronoun
-            word_updater.add(token, inflector.plural(token.text))
-
-
-
-    # token.morph: Case=Nom|Gender=Fem|Number=Sing|Person=3|PronType=Prs
-    # print(token.tag_, token.text, 222, token.morph)
-    # print(token.morph, token._.inflect(following_token.tag_, ))
-    # tag = Penn Treebank tag,
-
-
-def do_coref():
     coref_chains = doc._.coref_chains
     # coref_chains.print()
 
@@ -147,35 +95,32 @@ def do_coref():
         # else:
         #     print([doc[el.root_index].text for el in  chain])
 
-
     for token in found_character_tokens:
         if token.text == character:
             continue
         update_token(token)
 
-    word_updater.update()
-
-do_coref()
+    return word_updater.update(original_html)
 
 
-def do_all():
-    coref_chains = doc._.coref_chains
-    # coref_chains.print()
+dir = 'aliceInWonderland'
+files = os.listdir(dir)
+files = [f for f in files]
+path = os.getcwd()
 
-    found_character_tokens = []
+new_dir = dir + '_they'
+if not os.path.isdir(new_dir):
+    os.makedirs(new_dir, exist_ok=True)
 
-    for token in doc:
-        outcome = update_token(token)
-
-    for token in found_character_tokens:
-        update_token(token)
-
-    word_updater.update()
-
-
-# do_all()
-
-print(html)
+for file in tqdm(files,  desc ="Translating"):
+    if 'htm.xhtml' in file:
+        html = (open(dir + '/' + file, "r", encoding='utf-8')).read()
+        outcome = translate(html, update_params={'span': True, 'character': {'from': 'Alice', 'to': 'Alex'}})
+        file = open(new_dir + '/' + file, 'w', encoding='utf-8')
+        file.write(outcome)
+        file.close()
+    else:
+        shutil.copyfile(path + '/' + dir + '/' + file, path + '/' + new_dir + '/' + file)
 
 
 
